1ea63d2ebea900086ee160705f1306d71ac9fbdf
polarbear316 <polarcodingproductions@gmail.com>
2015-06-03 23:53:45 -0400

    Added select-ctrl

--- build/ctrls.js
+++ build/ctrls.js
@@ -168,3 +168,57 @@
 		}
 	});
 })();
+(function(){
+	Polymer({
+		is: "select-ctrl",
+		created:function(){},
+		ready:function(){
+			var self = this;
+			this.select  = this.$$("select");
+			this.combo  = this.$$(".combo-box");
+			
+			this.type = this.getAttribute("type") || "select";
+			
+			if(this.type == "select"){
+				var opts = this.combo.querySelectorAll("option");
+				[].forEach.call(opts,function(opt){
+					self.select.appendChild(opt);
+				});
+				this.$$(".combo-box").remove();
+			} else {
+				this.select.remove();
+				this.input = this.$$("input");
+				this.input.addEventListener("input",this.filter.bind(this));
+			}
+		},
+		factoryImpl:function(val){
+			this.value = val;
+		},
+		filter:function(e){
+			var val = e.target.value;
+			console.log(val);
+			var opts = this.combo.querySelectorAll("option");
+			[].forEach.call(opts,function(opt){
+				if(val.length && opt.textContent.toLowerCase().indexOf(val.toLowerCase()) == -1){
+					opt.style.display = "none";
+				} else {
+					opt.style.display = "block";					
+				}
+			});
+		},
+		get value(){
+			var selected = this.type == "combo" ? this.combo.querySelector("input") : this.select.querySelector("option[selected]");
+			return selected ? selected.value : "";
+		},
+		set value(val){
+			if(this.type == "combo"){
+				this.combo.querySelector("input").value = val;
+			} else {
+				var opt = this.select.querySelector('option[value="'+val+'"]');
+				if(opt){
+					opt.selected = true;
+				}
+			}
+		}
+	});
+})();
