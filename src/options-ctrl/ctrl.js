dd94b5327997f632766eab3bbf05c56482ac81d8
polarbear316 <polarcodingproductions@gmail.com>
2015-06-05 04:16:34 -0400

    added radio option to options-ctrl

--- src/options-ctrl/ctrl.js
+++ src/options-ctrl/ctrl.js
@@ -3,35 +3,71 @@
 		is: "options-ctrl",
 		created:function(){},
 		ready:function(){
-			var name = this.getAttribute("name");
+			var self = this,
+				name = this.getAttribute("name");
+				this.type = this.getAttribute("type") || "checkbox";
 			var opts = this.$$(".options-container").querySelectorAll("options-item");
 			[].forEach.call(opts,function(item){
 				item.setAttribute("name",name);
+				if(self.type=="checkbox" || self.type=="radio"){
+					item.setAttribute("type",self.type);
+				}
 			});
 		},
 		factoryImpl:function(val){
 			this.value = val;
 		},
 		get value(){
-			return this.$$("input").value;
+			var opts = this.$$(".options-container").querySelectorAll("options-item");
+			var vals = [].reduce.call(opts,function(out,item){
+				if(item.checked){
+					 out.push(item.value);
+				}
+				return out;
+			},[]);
+			if(this.type=="radio"){
+				return vals[0]; 
+			} 
+			return vals;
 		},
-		set value(val){
-			this.$$("input").value = val;
+		set value(options){
+			var cont = this.$$(".options-container");
+			cont.innerHTML = "";
+			options.forEach(function(option){
+				var item = new OptionItem(option);
+				cont.appendChild(item);
+			});
 		}
 	});
-	Polymer({
+	
+	var OptionItem = Polymer({
 		is: "options-item",
 		created:function(){},
 		ready:function(){
 			this.$$("input").id = this.getAttribute("name").replace(/[^a-zA-Z0-9]/g,"") + parseInt(Math.random()*1000,10);
 			this.$$("label").setAttribute("for",this.$$("input").id);
-			
+			this.$$("input").setAttribute("type",this.getAttribute("type")||"checkbox");
+			this.$$("input").setAttribute("value",this.getAttribute("value"));
 			if(!this.hasAttribute("icon")){
 				this.$$("img").remove();
 			}
+		}, 
+		factoryImpl:function(opt){
+			this.$$("input").id = opt.name.replace(/[^a-zA-Z0-9]/g,"") + parseInt(Math.random()*1000,10);
+			this.$$("label").setAttribute("for",this.$$("input").id);
+			this.$$("input").setAttribute("type",opt.type||"checkbox");
+			this.$$("input").setAttribute("value",opt.value);
+			this.$$("input").checked = opt.checked;
+			if(!opt.icon){
+				this.$$("img").remove();
+			}
 		},
-		factoryImpl:function(val){
-			this.value = val;
+		get value() {
+			this.$$("input").value;
+		},
+		set value(val) {
+			this.$$("input").setAttribute("value",val);
+			this.$$("input").value = val;
 		},
 		get checked(){
 			return this.$$("input").checked;
